48dfec3304512edf5f1a678b692766ef8ad3dbfc
sinjiwon <sjwon113@naver.com>
2023-03-15 19:40:07 +0900

    [Android] Glide로 이미지 Load 하기

---
+++ app/src/main/java/com/example/example/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.example;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView gifView = findViewById(R.id.iv_gif);
+
+//        Glide.with(this).load(R.drawable.gif).into(gifView);
+
+
+        Glide.with(this)
+                .asGif()
+                .load(R.raw.gif)
+                .listener(new RequestListener<GifDrawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                        if (resource != null) {
+                            resource.setLoopCount(1);
+                        }
+                        return false;
+                    }
+                })
+                .into(gifView);
+
+    }
+}
