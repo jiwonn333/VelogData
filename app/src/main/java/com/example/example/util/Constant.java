2daec4efda33918e5a945c01915919ca33aa775a
shinjiwon <sjwon113@naver.com>
2023-08-27 16:33:03 +0900

    refactor: Constant Convert Java to Kotlin

--- app/src/main/java/com/example/example/util/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.example.example.util;
-
-public class Constant {
-    public static final String GITHUB_URL = "https://api.github.com/";
-    public static final String BASE_URL = "http://jsonplaceholder.typicode.com";
-}
---
+++ app/src/main/java/com/example/example/util/Constant.kt
@@ -0,0 +1,6 @@
+package com.example.example.util;
+
+object Constant {
+    const val GITHUB_URL = "https://api.github.com/"
+    const val BASE_URL = "http://jsonplaceholder.typicode.com"
+}
