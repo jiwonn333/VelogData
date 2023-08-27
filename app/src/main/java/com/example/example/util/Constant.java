0bc8ba812528eae849d58b71e45d53c5f511c486
shinjiwon <sjwon113@naver.com>
2023-08-27 16:29:56 +0900

    refactor: token 값 local.properties에 관리

--- app/src/main/java/com/example/example/util/Constant.java
+++ app/src/main/java/com/example/example/util/Constant.java
@@ -2,7 +2,5 @@ package com.example.example.util;
 
 public class Constant {
     public static final String GITHUB_URL = "https://api.github.com/";
-    public static final String TOKEN = "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";
-
     public static final String BASE_URL = "http://jsonplaceholder.typicode.com";
 }
