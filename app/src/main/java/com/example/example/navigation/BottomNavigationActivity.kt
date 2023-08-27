a646aff5305e063e49229710177d5e5b3e9dd7e3
shinjiwon <sjwon113@naver.com>
2023-08-27 18:41:53 +0900

    refactor: BottomNavigationActivity Convert Java to Kotlin

--- app/src/main/java/com/example/example/navigation/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/example/navigation/BottomNavigationActivity.kt
@@ -1,27 +1,22 @@
-package com.example.example.navigation;
+package com.example.example.navigation
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.example.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.example.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-public class BottomNavigationActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bottom_navigation);
+class BottomNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
 
         // bottom navigation 탐색 설정
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
-        NavController navController = navHostFragment.getNavController();
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_view);
-        NavigationUI.setupWithNavController(bottomNav, navController);
-
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        setupWithNavController(bottomNav, navController)
     }
 }
